607ee1168e339c8b6574b47f377156125a05ee3c
Kevin Earls <kearls@redhat.com>
2019-12-22 18:38:42 +0100

    Day 4 part 2
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- DAY4/guess_test.go
+++ DAY4/guess_test.go
@@ -14,7 +14,7 @@ func TestGuessPassword(t *testing.T) {
 
 	for i := min; i <= max; i++ {
 		s := strconv.Itoa(i)
-		hasMatch := hasDigitMatch(s)
+		hasMatch := hasDigitMatch2(s)
 		increasing := digitsAreIncreasing(s)
 
 		if hasMatch && increasing {
@@ -23,15 +23,35 @@ func TestGuessPassword(t *testing.T) {
 	}
 
 	fmt.Printf("COUNT: %d\n", count)
-	assert.Equal(t, 1767, count)
+	//assert.Equal(t, 1767, count)  // For Part 1
+	assert.Equal(t, 1192, count)  // For Part 2
+
 }
 
+// This is for part 1
 func hasDigitMatch(target string) bool {
 	hasMatch := false
 	for j:= 0; j < len(target) -1; j++ {
 		if target[j] == target[j+1] {
 			hasMatch = true
 			break
+		}
+	}
+
+	return hasMatch
+}
+
+// This is for part 2
+func hasDigitMatch2(target string) bool {
+	hasMatch := false
+	for j:= 0; j < len(target) -1; j++ {
+		if target[j] == target[j+1] {
+
+			// Peek one character behind or ahead to make sure we only have 2 of the same in a row
+			if (j == 0 || target[j-1] != target[j]) && (j >= len(target)-2 || target[j+1] != target[j+2]) {
+				hasMatch = true
+				break
+			}
 		}
 	}
 
@@ -49,3 +69,11 @@ func digitsAreIncreasing(target string) bool {
 
 	return increasing
 }
+
+func TestHasDigitMatch2(t *testing.T) {
+	assert.True(t, hasDigitMatch2("112233"), "First")
+	assert.False(t, hasDigitMatch2("123444"), "second")
+	assert.True(t, hasDigitMatch2("111122"), "Third")
+}
+
+
